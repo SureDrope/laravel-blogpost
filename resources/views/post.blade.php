aa1b3fb1c839981fdab565abdfa9b5a8ee39aa8e
SureDrope <SureDrope@github.com>
2023-03-24 22:28:51 +0300

    Posts now contain a slug, which is used to access a certain post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,9 @@
 <x-layout>
     <article>
         <h1>{{ $post->title }}</h1>
-         {!! $post->body !!}
+        <p>
+        {!! $post->body !!}
+        </p>
     </article>
     <a href="/">Go back</a>
 </x-layout>
