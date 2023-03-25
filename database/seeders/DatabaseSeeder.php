a6d9b16312903f619b354128115d32c6b5c5ea81
SureDrope <SureDrope@github.com>
2023-03-25 20:32:53 +0300

    Add factories for Category and Post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,54 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user  = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
-         ]);
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => '1',
-            'slug' => '1',
-            'excerpt' => 'etetetet',
-            'body' => 'lorem ipsum hui sosisum'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => '2',
-            'slug' => '2',
-            'excerpt' => 'etetetet',
-            'body' => 'lorem ipsum hui sosisum'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => '3',
-            'slug' => '3',
-            'excerpt' => 'etetetet',
-            'body' => 'lorem ipsum hui sosisum'
-        ]);
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Post::factory(5)->create();
     }
 }
