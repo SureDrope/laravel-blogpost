da1e9ae447b004e3edbbc0857b03ec0f2ec2dbb6
SureDrope <SureDrope@github.com>
2023-03-27 11:01:27 +0300

    Work with categories

--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,21 @@ use App\Models\Post;
 
 
 Route::get('/', fn() => view('posts', [
-    'posts' => Post::latest()->with(['category', 'author'])->get()
+    'posts' => Post::latest()->with(['category', 'author'])->get(),
+    'categories' => Category::all()
 ]));
 
 Route::get('posts/{post:slug}', fn(Post $post) => view('post', [
     'post' => $post
 ]));
 
-Route::get('categories/{category:slug}', fn(Category $category) => view('posts',
-    ['posts' => $category->posts]
-));
+Route::get('categories/{category:slug}', fn(Category $category) => view('posts', [
+    'posts' => $category->posts,
+    'categories' => Category::all(),
+    'currentCategory' => $category
+]));
 
 Route::get('authors/{author:username}', fn(User $author) => view('posts', [
-    'posts' => $author->posts]
-));
+    'posts' => $author->posts,
+    'categories' => Category::all()
+]));
