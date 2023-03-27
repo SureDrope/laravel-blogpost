05095a8a570479c2d1a7624407e0ea3b201c2b49
SureDrope <SureDrope@github.com>
2023-03-27 21:02:34 +0300

    Dropdown improvements

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Models\Post;
 Route::get('/', fn() => view('posts', [
     'posts' => Post::latest()->with(['category', 'author'])->get(),
     'categories' => Category::all()
-]));
+]))->name('home');
 
 Route::get('posts/{post:slug}', fn(Post $post) => view('post', [
     'post' => $post
@@ -19,7 +19,7 @@ Route::get('categories/{category:slug}', fn(Category $category) => view('posts',
     'posts' => $category->posts,
     'categories' => Category::all(),
     'currentCategory' => $category
-]));
+]))->name('category');
 
 Route::get('authors/{author:username}', fn(User $author) => view('posts', [
     'posts' => $author->posts,
