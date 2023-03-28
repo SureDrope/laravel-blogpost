be9664f8eb9b54f19fee6a053c7344f7a714ca31
SureDrope <SureDrope@github.com>
2023-03-28 12:27:06 +0300

    Messy post search

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,18 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 
 
-Route::get('/', fn() => view('posts', [
-    'posts' => Post::latest()->with(['category', 'author'])->get(),
-    'categories' => Category::all()
-]))->name('home');
+Route::get('/', function () {
+    $posts = Post::latest()->with(['category', 'author']);
+    if (request('search')) {
+        $posts
+            ->where('title', 'like', '%' . request('search') . '%')
+            ->orWhere('body', 'like', '%' . request('search') . '%');
+    }
+    return view('posts', [
+        'posts' => $posts->get(),
+        'categories' => Category::all()
+    ]);
+})->name('home');
 
 Route::get('posts/{post:slug}', fn(Post $post) => view('post', [
     'post' => $post
