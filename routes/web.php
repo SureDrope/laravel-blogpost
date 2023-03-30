d2482cb4fd02c4fe9fa702a9ba6aa59782fa2275
SureDrope <SureDrope@github.com>
2023-03-30 16:10:24 +0300

    Add search based on an author, remove authors/* uri, replaced with /?author=

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,3 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-//Route::get('categories/{category:slug}', fn(Category $category) => view('posts', [
-//    'posts' => $category->posts,
-//    'categories' => Category::all(),
-//    'currentCategory' => $category
-//]))->name('category');
-
-Route::get('authors/{author:username}', fn(User $author) => view('posts', [
-    'posts' => $author->posts
-]));
