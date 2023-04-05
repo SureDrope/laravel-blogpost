f6717198a61e4719e5624a3226857d7d3ea89197
SureDrope <SureDrope@github.com>
2023-04-05 20:06:01 +0300

    Add comment form

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,8 @@
                 <img src="/images/illustration-1.png" alt="" class="rounded-xl">
 
                 <p class="mt-4 block text-gray-400 text-xs">
-                    Published <time> {{ $post->created_at->diffForHumans() }}</time>
+                    Published
+                    <time> {{ $post->created_at->diffForHumans() }}</time>
                 </p>
 
                 <div class="flex items-center lg:justify-center text-sm mt-4">
@@ -38,7 +39,7 @@
                     </a>
 
                     <div class="space-x-2">
-{{--                        <x-category-button :category="$post->category"/>--}}
+                        {{--                        <x-category-button :category="$post->category"/>--}}
                     </div>
                 </div>
 
@@ -52,8 +53,34 @@
             </div>
 
             <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                <x-panel>
+                    <form method="POST" action="#">
+                        @csrf
+                        <header class="flex items-center">
+                            <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="40" height="40"
+                                 class="rounded-full">
+                            <h2 class="ml-4">
+                                Want to cuc?
+                            </h2>
+                        </header>
+                        <div class="mt-5">
+                        <textarea name="body" rows="5" placeholder="Your comment"
+                                  class="w-full text-sm focus:outline-none focus:ring"
+                        ></textarea>
+                        </div>
+                        <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
+                            <button type="submit"
+                                    class="bg-blue-500 text-white uppercase
+                                        font-semibold text-xs py-2 px-10
+                                        rounded-2xl hover:bg-blue-600">
+
+                                Post
+                            </button>
+                        </div>
+                    </form>
+                </x-panel>
                 @foreach($post->comments as $comment)
-                    <x-post-comment :comment="$comment" />
+                    <x-post-comment :comment="$comment"/>
                 @endforeach
             </section>
 
