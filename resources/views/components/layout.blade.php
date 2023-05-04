864255705564500709a8b6ed1cc539433d44c015
SureDrope <SureDrope@github.com>
2023-05-04 17:57:52 +0400

    cleaning

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 
 <head>
-    <title>Laravel From Scratch Blog</title>
+    <title>Blog</title>
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
@@ -18,7 +18,7 @@
     <nav class="md:flex md:justify-between md:items-center">
         <div>
             <a href="/">
-                <img src="/images/logo.svg" alt="Laracasts Logo" width="165" height="16">
+                <h1>Home</h1>
             </a>
         </div>
 
